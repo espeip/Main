cb99ba40e614283dd9812182cb8cc8d2f7a91b40
gespeip <gespeip@gmail.com>
2018-06-02 13:56:30 +0300

    Закоментирована строка

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-	List<Integer> list = new ArrayList<>();
+	//List<Integer> list = new ArrayList<>();
         System.out.println("Hello! " + args[0]);// write your code here
     }
 }
