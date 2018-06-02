da1543c480c2eaae3a4774c05417d178a20f3d98
gespeip <gespeip@gmail.com>
2018-06-02 14:28:27 +0300

    Добавлена копия Main

---
+++ Main - Copy.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//List<Integer> list = new ArrayList<>();
+        System.out.println("Hello! " + args[0]);// write your code here
+    }
+}
